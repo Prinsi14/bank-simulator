c2a3878eec332e25bf203b1782d230457a326351
Prinsi Dwivedi <prinsi75dwivedi@gmail.com>
2026-06-13 15:29:55 +0530

    added create_account function

--- bank.py
+++ bank.py
@@ -1,1 +1,27 @@
-print("welcom to python bank")
+accounts = {}
+
+def create_account():
+    name = input("Enter your name: ")
+
+    account_id = name.lower().replace(" ", "_")
+
+    if account_id in accounts:
+        print("Account already exists!")
+        return 
+
+    balance = float(input("Enter initial deposit amount: "))
+
+    accounts[account_id] = { 
+        "name": name,
+        "balance": balance,
+        "transactions": []
+    }  
+
+    print("Account created successfully!")
+    print("Your account id is:", account_id)
+
+
+create_account()
+
+
+
---
+++ create_account.py
@@ -0,0 +1,2 @@
+name = input("enter your name")
+print("yo entered:", name)
